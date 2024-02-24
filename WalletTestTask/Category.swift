7d58dec49f2d4783feecb09845045cd8938a95e2
= <=>
2024-02-24 13:40:39 +0200

    Created Transaction Entity, App Icon and LaunchScreen

---
+++ WalletTestTask/Category.swift
@@ -0,0 +1,16 @@
+//
+//  Category.swift
+//  WalletTestTask
+//
+//  Created by Анастасія Грисюк on 24.02.2024.
+//
+
+import Foundation
+
+enum Category {
+    case groceries
+    case taxi
+    case electronics
+    case restaurant
+    case other
+}
